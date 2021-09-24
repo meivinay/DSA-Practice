3a2d1203ece387b86cd65580c5f6100a054fa071
meivinay <tanwarv96@gmail.com>
2021-09-24 12:23:43 +0530

    141

---
+++ LeetCode/CycleInLinkedList.java
@@ -0,0 +1,17 @@
+   public boolean hasCycle(ListNode head) {
+        if(head == null){
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while( fast.next != null  && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                return true;
+            }
+        }
+        
+        return false;
+    }
